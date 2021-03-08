5a6bcb0cf04a0f6447e9667d7d05bf15245798f7
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-03-08 18:12:00 +0300

    add install server script

---
+++ install-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# This script installs all the tools I use on servers and deploys
+# configuration files for them on Arch Linux or other Arch-based distributions
+
+# Should be ran from repo directory
+# ----- aliases -----
+echo "Installing aliases"
+mkdir -p ~/.config
+cp aliasrc ~/.config
+
+# ----- man pages -----
+echo "Installing man pages..."
+sudo pacman -S --noconfirm --needed man-db
+
+# ----- shell -----
+echo "Installing shell..."
+sudo pacman -S --noconfirm --needed zsh zsh-syntax-highlighting
+cp .bashrc .zshrc .bash_profile .zprofile .profile ~
+chsh -s /usr/bin/zsh
+
+# ----- system info -----
+echo "Installing system info..."
+sudo pacman -S --noconfirm --needed neofetch
+mkdir -p ~/.config
+cp -r neofetch ~/.config
+
+# ----- text editor -----
+echo "Installing text editor..."
+sudo pacman -S --noconfirm --needed vim curl python
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp .vimrc ~
+vim +PlugInstall +qall
+source ~/.vimrc
+
+# ----- version control system -----
+echo "Installing version control system..."
+sudo pacman -S --noconfirm --needed git
+cp .gitconfig ~
--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # This script installs all the tools I use and deploys configuration 
-# files for them on Arch Linux or other Arch-based distributive
+# files for them on Arch Linux or other Arch-based distributions
 
 # Should be ran from repo directory
 
-# ----- aliases -----
-echo "Installing aliases"
-mkdir -p ~/.config
-cp aliasrc ~/.config
+# ----- CLI-only tools (used on servers) -----
+./install-server.sh
 
 # ----- browser -----
 echo "Installing browser..."
@@ -28,44 +26,9 @@ cp -r ranger ~/.config
 mkdir -p ~/.local/share/fonts
 cp fonts/* ~/.local/share/fonts
 
-# ----- version control system -----
-echo "Installing version control system..."
-sudo pacman -S --noconfirm --needed git
-cp .gitconfig ~
-
-# ----- man pages -----
-echo "Installing man pages..."
-sudo pacman -S --noconfirm --needed man-db
-
-# ----- shell -----
-echo "Installing shell..."
-sudo pacman -S --noconfirm --needed zsh zsh-syntax-highlighting
-cp .bashrc .zshrc .bash_profile .zprofile .profile ~
-chsh -s /usr/bin/zsh
-
-# ----- system info -----
-echo "Installing system info..."
-sudo pacman -S --noconfirm --needed neofetch
-mkdir -p ~/.config
-cp -r neofetch ~/.config
-
 # ----- terminal emulator -----
 echo "Installing terminal emulator..."
 sudo pacman -S ---noconfirm --needed rxvt-unicode
-
-# ----- text editor -----
-echo "Installing text editor..."
-sudo pacman -S --noconfirm --needed vim curl python
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp .vimrc ~
-vim +PlugInstall +qall
-source ~/.vimrc
-
-# ----- version control system -----
-echo "Installing version control system..."
-sudo pacman -S --noconfirm --needed git
-cp .gitconfig ~
 
 # ----- window manager -----
 echo "Installing window manager..."
