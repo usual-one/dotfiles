6d2cc66b9d50fc1b061e5ca1677ab8e53f6e3e41
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-03-08 15:56:41 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# This script installs all the tools I use and deploys configuration 
+# files for them on Arch Linux or other Arch-based distributive
+
+# Should be ran from repo directory with root priveleges
+
+# ----- browser -----
+echo "Installing browser..."
+pacman -S --noconfirm --needed firefox
+
+# ----- display server -----
+echo "Installing display server..."
+pacman -S --noconfirm --needed xorg-server xorg-xinit xorg-xrdb
+cp .xinitrc .Xresources ~
+xrdb ~/.Xresources
+
+# ----- version control system -----
+echo "Installing version control system..."
+pacman -S --noconfirm --needed git
+cp .gitconfig ~
+
+# ----- man pages -----
+echo "Installing man pages..."
+pacman -S --noconfirm --needed man-db
+
+# ----- shell -----
+echo "Installing shell..."
+pacman -S --noconfirm --needed zsh zsh-syntax-highlighting
+cp .bashrc .zshrc .bash_profile .zprofile .profile ~
+chsh -s /usr/bin/zsh
+
+# ----- terminal emulator -----
+echo "Installing terminal emulator..."
+pacman -S ---noconfirm --needed rxvt-unicode
+
+# ----- text editor -----
+echo "Installing text editor..."
+pacman -S --noconfirm --needed vim curl
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp .vimrc ~
+source ~/.vimrc
+vim +PlugInstall +qall
+
+# ----- version control system -----
+echo "Installing version control system..."
+pacman -S --noconfirm --needed git
+cp .gitconfig ~
+
